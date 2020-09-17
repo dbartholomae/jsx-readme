d880caefb86c25c0dfd338152d7f81ab96e5eb8b
Daniel Bartholomae <daniel@bartholomae.name>
2020-09-17 13:51:50 +0200

    test: explicitly run ts-node in e2e test

--- test/README.md.test.tsx
+++ test/README.md.test.tsx
@@ -1,8 +1,18 @@
 import * as fs from "fs";
-import "../examples/README.md.tsx";
+import * as child_process from "child_process";
 
 describe("README creation script", () => {
   it("creates the README file of the repo", async () => {
+    const childProcess = child_process.exec("ts-node ./examples/README.md.tsx");
+    await new Promise((resolve, reject) => {
+      childProcess.on("exit", (code) => {
+        if (code !== 0) {
+          return reject("Fork failed");
+        }
+        resolve();
+      });
+    });
+
     const expectedFile = fs.readFileSync("./test/README.expected.md", {
       encoding: "utf8",
     });
@@ -10,5 +20,5 @@ describe("README creation script", () => {
       encoding: "utf8",
     });
     expect(actualFile).toBe(expectedFile);
-  });
+  }, 10000);
 });
