c8c1cfefbcdac8bda0981916106e4c80e5be25bb
Daniel Bartholomae <daniel@bartholomae.name>
2020-09-17 14:53:12 +0200

    test(Badge): add test

---
+++ src/elements/Badge.test.tsx
@@ -0,0 +1,20 @@
+/* @jsx MD */
+import MD, { render } from "jsx-md";
+import { Badge } from "./Badge";
+
+describe("Badge", () => {
+  it("renders an npm package badge", () => {
+    expect(
+      render(
+        <Badge
+          to="https://npmjs.org/package/package-name"
+          imageSource="https://badge.fury.io/js/package-name.svg"
+        >
+          npm package
+        </Badge>
+      )
+    ).toBe(
+      "[![npm package](https://badge.fury.io/js/package-name.svg)](https://npmjs.org/package/package-name)\n"
+    );
+  });
+});
