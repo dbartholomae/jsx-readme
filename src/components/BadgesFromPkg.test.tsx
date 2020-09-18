9b44721d542db593d1c104bd0ac80894c30a913a
Daniel Bartholomae <daniel@bartholomae.name>
2020-09-19 01:14:03 +0200

    test(BadgesFromPkg): allow to set options

--- src/components/BadgesFromPkg.test.tsx
+++ src/components/BadgesFromPkg.test.tsx
@@ -3,8 +3,28 @@ import MD, { render } from "jsx-md";
 import { BadgesFromPkg } from "./BadgesFromPkg";
 import { badges, defaultBadges } from "./badges";
 import pkg from "../../test/package.json";
+import { GithubWorkflowBadge } from "./badges/GithubWorkflowBadge";
 
 describe("BadgesFromPkg", () => {
+  it("allows to set options for GithubWorkflowBadge", () => {
+    const overrideBadges = {
+      githubWorkflow: {
+        workflowName: "Test",
+      },
+    };
+
+    expect(
+      render(<BadgesFromPkg pkg={pkg} overrideBadges={overrideBadges} />)
+    ).toContain(
+      render(
+        <GithubWorkflowBadge
+          pkg={pkg}
+          options={overrideBadges.githubWorkflow}
+        />
+      )
+    );
+  });
+
   it.each(["npmVersion", "jsxReadme", "githubIssues"] as const)(
     "shows an %s badge by default",
     (badgeName) => {
