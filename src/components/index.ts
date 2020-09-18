44fa314ad8329c4706ab847e93f7611cd55cfb7d
Daniel Bartholomae <daniel@bartholomae.name>
2020-09-18 23:18:08 +0200

    fix: export all components

--- src/components/index.ts
+++ src/components/index.ts
@@ -2,6 +2,9 @@
  * @packageDocumentation
  * @internal
  */
+export * from "./badges";
+export { Badge } from "./Badge";
+export { BadgesFromPkg } from "./BadgesFromPkg";
 export { ExamplesFromPkg } from "./ExamplesFromPkg";
 export { HeaderFromPkg } from "./HeaderFromPkg";
 export { HomepageFromPkg } from "./HomepageFromPkg";
