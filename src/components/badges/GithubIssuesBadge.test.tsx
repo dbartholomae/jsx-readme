6c9f18170282908f548faad1de6bd24d54fa8b8d
Daniel Bartholomae <daniel@bartholomae.name>
2020-09-18 20:00:11 +0200

    test(GithubIssuesBadge): render if repository is an object

--- src/components/badges/GithubIssuesBadge.test.tsx
+++ src/components/badges/GithubIssuesBadge.test.tsx
@@ -4,10 +4,31 @@ import { GithubIssuesBadge } from "./GithubIssuesBadge";
 import { Badge } from "../Badge";
 
 describe("GithubIssuesBadge", () => {
-  it("shows a github-issues badge", () => {
+  it("shows a github-issues badge if repository is an ssh url", () => {
     const pkg = {
       name: "package-name",
       repository: "git@github.com:dbartholomae/jsx-readme.git",
+    };
+
+    expect(render(<GithubIssuesBadge pkg={pkg} />)).toContain(
+      render(
+        <Badge
+          imageSource="https://img.shields.io/github/issues-raw/dbartholomae/jsx-readme.svg"
+          link="https://github.com/dbartholomae/jsx-readme/issues"
+        >
+          open issues
+        </Badge>
+      )
+    );
+  });
+
+  it("shows a github-issues badge if repository is an ssh url in a repository object", () => {
+    const pkg = {
+      name: "package-name",
+      repository: {
+        type: "git",
+        url: "git@github.com:dbartholomae/jsx-readme.git",
+      },
     };
 
     expect(render(<GithubIssuesBadge pkg={pkg} />)).toContain(
