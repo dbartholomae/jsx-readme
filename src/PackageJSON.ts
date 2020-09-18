72d4c680879228168712bebc75f175828c814715
Daniel Bartholomae <daniel@bartholomae.name>
2020-09-18 19:27:34 +0200

    feat(GithubIssuesBadge): extract owner and repo from repository entry

--- src/PackageJSON.ts
+++ src/PackageJSON.ts
@@ -3,4 +3,11 @@ export interface PackageJSON {
   homepage?: string;
   name: string;
   private?: boolean;
+  repository?: string | Repository;
+}
+
+export interface Repository {
+  type: string;
+  url: string;
+  directory?: string;
 }
--- src/components/badges/GithubIssuesBadge.tsx
+++ src/components/badges/GithubIssuesBadge.tsx
@@ -1,15 +1,35 @@
 /* @jsx MD */
 import MD, { Component } from "jsx-md";
 import { Badge } from "../Badge";
-import { PackageJSON } from "../../PackageJSON";
+import type { PackageJSON, Repository } from "../../PackageJSON";
 
 export interface Props {
   pkg: PackageJSON;
 }
 
-export const GithubIssuesBadge: Component<Props> = () => {
-  const owner = "dbartholomae";
-  const repoName = "jsx-readme";
+function extractGithubOwnerAndRepo(
+  repository?: Repository | string
+): [string, string] | undefined {
+  if (repository === undefined) {
+    return undefined;
+  }
+  const url = typeof repository === "string" ? repository : repository.url;
+  const ownerRegex = /^git@github\.com:([-\w]+)\/([-\w]+)\.git$/;
+  const regexResult = ownerRegex.exec(url);
+  if (regexResult === null) {
+    return undefined;
+  }
+  const owner = regexResult[1];
+  const repo = regexResult[2];
+  return [owner, repo];
+}
+
+export const GithubIssuesBadge: Component<Props> = ({ pkg }) => {
+  const ownerAndRepo = extractGithubOwnerAndRepo(pkg.repository);
+  if (ownerAndRepo === undefined) {
+    return null;
+  }
+  const [owner, repoName] = ownerAndRepo;
   return (
     <Badge
       link={`https://github.com/${owner}/${repoName}/issues`}
