fed78c2d3e373e6f95987586951435b8260d71c6
Daniel Bartholomae <daniel@bartholomae.name>
2020-09-20 20:52:12 +0200

    feat(ExampleFile): render new line after each example file

--- src/components/ExampleFile.tsx
+++ src/components/ExampleFile.tsx
@@ -1,6 +1,6 @@
 /* @jsx Md */
 import type { Component } from "jsx-md";
-import Md, { CodeBlock, Fragment, Heading } from "jsx-md";
+import Md, { CodeBlock, Fragment, Heading, LineBreak } from "jsx-md";
 
 interface Props {
   fileName: string;
@@ -16,5 +16,6 @@ export const ExampleFile: Component<Props> = ({
   <Fragment>
     <Heading level={3}>{fileName}</Heading>
     <CodeBlock language={language}>{children}</CodeBlock>
+    <LineBreak />
   </Fragment>
 );
