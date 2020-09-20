f34ad043517c9e6f9ebeb577543a82d41a2c611c
Daniel Bartholomae <daniel@bartholomae.name>
2020-09-20 19:27:32 +0200

    refactor(SemanticReleaseBadge): ask for package.json

--- src/components/badges/SemanticReleaseBadge.test.tsx
+++ src/components/badges/SemanticReleaseBadge.test.tsx
@@ -4,8 +4,14 @@ import { Badge } from "../Badge";
 import { SemanticReleaseBadge } from ".";
 
 describe("SemanticReleaseBadge", () => {
-  it("shows a semantic release badge", () => {
-    expect(render(<SemanticReleaseBadge />)).toContain(
+  it("shows a semantic release badge if semantic-release is a devDependency", () => {
+    const pkg = {
+      name: "package-name",
+      devDependencies: {
+        "semantic-release": "*",
+      },
+    };
+    expect(render(<SemanticReleaseBadge pkg={pkg} />)).toContain(
       render(
         <Badge
           imageSource="https://img.shields.io/badge/%20%20%F0%9F%93%A6%F0%9F%9A%80-semantic--release-e10079.svg"
--- src/components/badges/SemanticReleaseBadge.tsx
+++ src/components/badges/SemanticReleaseBadge.tsx
@@ -1,8 +1,13 @@
 /* @jsx MD */
 import MD, { Component } from "jsx-md";
 import { Badge } from "../Badge";
+import { PackageJSON } from "../../PackageJSON";
 
-export const SemanticReleaseBadge: Component = () => (
+interface Props {
+  pkg: Readonly<PackageJSON>;
+}
+
+export const SemanticReleaseBadge: Component<Props> = () => (
   <Badge
     link="https://github.com/semantic-release/semantic-release#badge"
     imageSource="https://img.shields.io/badge/%20%20%F0%9F%93%A6%F0%9F%9A%80-semantic--release-e10079.svg"
