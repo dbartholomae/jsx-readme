42269f001d6f9df9e03902c67a6ae0c0feaff840
Daniel Bartholomae <daniel@bartholomae.name>
2020-09-20 21:02:17 +0200

    test(ExamplesFromPkg): render the example files as ExampleFiles

--- src/components/ExamplesFromPkg.test.tsx
+++ src/components/ExamplesFromPkg.test.tsx
@@ -2,6 +2,7 @@
 import Md, { render } from "jsx-md";
 import { ExamplesFromPkg } from "./ExamplesFromPkg";
 import mockFs from "mock-fs";
+import { ExampleFile } from "./ExampleFile";
 
 describe("ExamplesFromPkg", () => {
   describe("with an examples directory with a json file", () => {
@@ -26,6 +27,16 @@ describe("ExamplesFromPkg", () => {
 
     it("renders an 'Examples' heading", () => {
       expect(render(<ExamplesFromPkg pkg={pkg} />)).toContain("## Examples\n");
+    });
+
+    it("renders the example.json file as an ExampleFile", () => {
+      expect(render(<ExamplesFromPkg pkg={pkg} />)).toContain(
+        render(
+          <ExampleFile fileName="example.json" language="json">
+            {"{}"}
+          </ExampleFile>
+        )
+      );
     });
   });
 });
