f478e244d9ac0d70f06072be531827cc284bbc37
Daniel Bartholomae <daniel@bartholomae.name>
2020-10-16 00:25:35 +0200

    refactor: use promise api of fs

--- src/components/ExamplesFromPkg.tsx
+++ src/components/ExamplesFromPkg.tsx
@@ -1,8 +1,8 @@
 /* @jsx Md */
 import type { Component } from "jsx-md";
-import Md, { Fragment, Heading } from "jsx-md";
+import Md, { awaitComponent, Fragment, Heading } from "jsx-md";
 import type { PackageJSON } from "../PackageJSON";
-import * as fs from "fs";
+import { promises as fs } from "fs";
 import * as path from "path";
 import { CodeFile } from "./CodeFile";
 
@@ -14,51 +14,55 @@ interface Props {
 }
 
 /** Show all files from the example directory defined in package.json.² */
-export const ExamplesFromPkg: Component<Props> = ({
-  pkg,
-  /** The encoding to use for loading all files. */
-  encoding = "utf8",
-  /** Whether to replace all imports from '..' with imports from 'package-name' */
-  replacePackageImportsWithPackageName = true,
-}) => {
-  const examplesFolder = pkg.directories?.example;
-  if (examplesFolder === undefined) {
-    return null;
+export const ExamplesFromPkg: Component<Props> = awaitComponent(
+  async ({
+    pkg,
+    /** The encoding to use for loading all files. */
+    encoding = "utf8",
+    /** Whether to replace all imports from '..' with imports from 'package-name' */
+    replacePackageImportsWithPackageName = true,
+  }) => {
+    const examplesFolder = pkg.directories?.example;
+    if (examplesFolder === undefined) {
+      return null;
+    }
+
+    const filenames = await fs.readdir(examplesFolder);
+    const examples = await Promise.all(
+      filenames.map(async (fileName) => ({
+        fileName,
+        content: await fs.readFile(path.join(examplesFolder, fileName), {
+          encoding,
+        }),
+      }))
+    );
+
+    const replacements = replacePackageImportsWithPackageName
+      ? [
+          {
+            find: /from ".."/g,
+            replace: `from "${pkg.name}"`,
+          },
+        ]
+      : [];
+
+    return (
+      <Fragment>
+        <Heading level={2}>🔬 Examples</Heading>
+        {examples
+          .sort(({ fileName: fileNameA }, { fileName: fileNameB }) =>
+            fileNameA.localeCompare(fileNameB, "en", { sensitivity: "base" })
+          )
+          .map(({ fileName, content }) => (
+            <CodeFile
+              key={fileName}
+              fileName={fileName}
+              replacements={replacements}
+            >
+              {content}
+            </CodeFile>
+          ))}
+      </Fragment>
+    );
   }
-
-  const filenames = fs.readdirSync(examplesFolder);
-  const examples = filenames.map((fileName) => ({
-    fileName,
-    content: fs.readFileSync(path.join(examplesFolder, fileName), {
-      encoding,
-    }),
-  }));
-
-  const replacements = replacePackageImportsWithPackageName
-    ? [
-        {
-          find: /from ".."/g,
-          replace: `from "${pkg.name}"`,
-        },
-      ]
-    : [];
-
-  return (
-    <Fragment>
-      <Heading level={2}>🔬 Examples</Heading>
-      {examples
-        .sort(({ fileName: fileNameA }, { fileName: fileNameB }) =>
-          fileNameA.localeCompare(fileNameB, "en", { sensitivity: "base" })
-        )
-        .map(({ fileName, content }) => (
-          <CodeFile
-            key={fileName}
-            fileName={fileName}
-            replacements={replacements}
-          >
-            {content}
-          </CodeFile>
-        ))}
-    </Fragment>
-  );
-};
+);
--- src/renderToFile.test.tsx
+++ src/renderToFile.test.tsx
@@ -1,7 +1,7 @@
 /* @jsx Md */
 import Md, { Fragment } from "jsx-md";
 import mockFs from "mock-fs";
-import * as fs from "fs";
+import { promises as fs } from "fs";
 import { renderToFile } from "./renderToFile";
 
 describe("renderToFile", () => {
@@ -16,7 +16,7 @@ describe("renderToFile", () => {
   it("renders a component to a file", async () => {
     const filename = "README.md";
     await renderToFile(filename, <Fragment>Content</Fragment>);
-    const actualFile = fs.readFileSync(filename, { encoding: "utf-8" });
+    const actualFile = await fs.readFile(filename, { encoding: "utf-8" });
     expect(actualFile).toBe("Content");
   });
 
