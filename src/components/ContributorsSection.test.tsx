78925ecdd4e029ab954e6effdd7f15cc4eb6126a
junwen-k <junwenkwan97@gmail.com>
2021-10-02 22:30:48 +0800

    feat(ContributorsSection): rework ContributorSection to a pure rendering component

--- src/components/ContributorsSection.test.tsx
+++ src/components/ContributorsSection.test.tsx
@@ -5,35 +5,74 @@ import { ContributorsSection } from "./ContributorsSection";
 
 describe("ContributorsSection", () => {
   it("renders a 'Contributors' heading", async () => {
-    const pkg = {
-      name: "package-name",
-      repository: "github:dbartholomae/test-repo",
-    };
-
     expect(
-      await render(<ContributorsSection title="Contributors" pkg={pkg} />)
+      await render(
+        <ContributorsSection
+          contributors={[]}
+          title="Contributors"
+          owner="dbartholomae"
+          repo="test-repo"
+        />
+      )
     ).toContain("## Contributors");
 
     expect(
-      await render(<ContributorsSection pkg={pkg} />)
+      await render(
+        <ContributorsSection
+          contributors={[]}
+          owner="dbartholomae"
+          repo="test-repo"
+        />
+      )
     ).toContain("## ‍💼 Contributors");
   });
 
   it("renders sentence with a link to the contributors on GitHub", async () => {
-    const pkg = {
-      name: "package-name",
-      repository: "github:dbartholomae/test-repo",
-    };
-    expect(await render(<ContributorsSection pkg={pkg} />)).toContain(
+    expect(
+      await render(
+        <ContributorsSection
+          contributors={[]}
+          owner="dbartholomae"
+          repo="test-repo"
+        />
+      )
+    ).toContain(
       "This package only works thanks to [all of our contributors](https://github.com/dbartholomae/test-repo/graphs/contributors)"
     );
   });
 
-  it("renders nothing when repository is not hosted on GitHub", async () => {
-    const pkg = {
-      name: "package-name",
-      repository: "bitbucket:user/test-repo",
-    };
-    expect(await render(<ContributorsSection pkg={pkg} title="Contributors" />)).toBe("")
+  it("renders a list of contributors on GitHub", async () => {
+    const contributors = [
+      {
+        id: 1,
+        name: "dbartholomae",
+      },
+    ];
+    expect(
+      await render(
+        <ContributorsSection
+          contributors={contributors}
+          owner="dbartholomae"
+          repo="test-repo"
+        />
+      )
+    ).toContain(
+      '[![dbartholomae](https://avatars.githubusercontent.com/u/1?size=100 "dbartholomae")](https://github.com/dbartholomae "dbartholomae\'s GitHub profile\'")'
+    );
+  });
+
+  it("renders a show more contributors link when additional contributors count is provided", async () => {
+    expect(
+      await render(
+        <ContributorsSection
+          contributors={[]}
+          owner="dbartholomae"
+          repo="test-repo"
+          additionalContributorsCount={1}
+        />
+      )
+    ).toContain(
+      "[+ 1 contributors](https://github.com/dbartholomae/test-repo/graphs/contributors)"
+    );
   });
 });
--- src/components/ContributorsSection.tsx
+++ src/components/ContributorsSection.tsx
@@ -1,33 +1,47 @@
 /* @jsx MD */
 import { Component } from "jsx-md";
 import MD, { Fragment, Heading, LineBreak, Link, Text } from "jsx-md";
-import { PackageJSON } from "../PackageJSON";
-import { extractGithubOwnerAndRepo } from "./badges/utils/extractGithubOwnerAndRepo";
+import { ContributorsList } from "./ContributorsList";
+import type { Contributor } from "./ContributorsList";
 
 /** @internal */
 interface Props {
   title?: string;
-  pkg: PackageJSON;
+  repo: string;
+  owner: string;
+  contributors: Contributor[];
+  additionalContributorsCount?: number;
 }
 /** Displays a contributors section. */
 export const ContributorsSection: Component<Props> = ({
   title = "‍💼 Contributors",
-  pkg,
-}: Props) => {
-  const repoData = extractGithubOwnerAndRepo(pkg.repository);
-  if (repoData === undefined) {
-    return null;
-  }
-
-  const { repo, owner } = repoData;
-
-  return (
-    <Fragment>
-      <Heading level={2}>{title}</Heading>
-      <Text>This package only works thanks to</Text>{" "}
-      <Link to={`https://github.com/${owner}/${repo}/graphs/contributors`}>all of our contributors</Link>.
-      <LineBreak />
-      <LineBreak />
-    </Fragment>
-  );
-};
+  repo,
+  owner,
+  contributors,
+  additionalContributorsCount,
+}) => (
+  <Fragment>
+    <Heading level={2}>{title}</Heading>
+    <Text>This package only works thanks to</Text>{" "}
+    <Link to={`https://github.com/${owner}/${repo}/graphs/contributors`}>
+      all of our contributors
+    </Link>
+    .
+    <LineBreak />
+    <LineBreak />
+    <ContributorsList contributors={contributors} />
+    {additionalContributorsCount && (
+      <Fragment>
+        <LineBreak />
+        <LineBreak />
+        <Link to={`https://github.com/${owner}/${repo}/graphs/contributors`}>
+          {`+ ${additionalContributorsCount.toLocaleString(
+            "en-US"
+          )} contributors`}
+        </Link>
+      </Fragment>
+    )}
+    <LineBreak />
+    <LineBreak />
+  </Fragment>
+);
